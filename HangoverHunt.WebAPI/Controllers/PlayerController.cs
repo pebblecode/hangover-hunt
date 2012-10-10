56f3d974840c3822792d11aa36e19016dcda41cc
daniel <daniel@danielbradley.net>
2012-10-10 11:27:32 +0100

    Implemented simple web API.

---
+++ HangoverHunt.WebAPI/Controllers/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HangoverHunt.WebAPI.Controllers
+{
+    public class PlayerController : Controller
+    {
+        [HttpGet]
+        public ActionResult GetQuestion()
+        {
+            return Json(new { Question = "What is the answer?" }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult CheckResult(string answer)
+        {
+            return Json(false);
+        }
+    }
+}
