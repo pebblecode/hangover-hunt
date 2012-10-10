2d502c717757cb4e51118325e2ed7e31be08b997
daniel <daniel@danielbradley.net>
2012-10-10 11:30:44 +0100

    Upgraded Web API to also have MVC views etc.

--- HangoverHunt.WebAPI/Properties/AssemblyInfo.cs
+++ HangoverHunt.WebAPI/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a0dfc46b-008d-44e5-9dfa-4528b685dc8f")]
+[assembly: Guid("b7a3aeb8-2089-4032-bab5-27612c85eb2e")]
 
 // Version information for an assembly consists of the following four values:
 //
