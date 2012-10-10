4f0632df37852a8eae298b013b48cc06fe10f91b
daniel <daniel@danielbradley.net>
2012-10-10 12:48:51 +0100

    Created a hunt state object to manage the chaining of riddles.

--- HangoverHunt/Hunt.cs
+++ HangoverHunt/Hunt.cs
@@ -15,20 +15,56 @@ namespace HangoverHunt
             riddles.Add(riddle);
         }
 
-        public IRiddle GetCurrentRiddle()
+        public IRiddle CurrentRiddle
         {
-            if (currentRiddle < riddles.Count)
+            get
+            {
+                if (State != HuntState.InProgress)
+                    return null;
+
                 return riddles[currentRiddle];
-            else
-                return null;
+            }
         }
 
         public bool CheckAnswer(IAnswer answer)
         {
-            var result = GetCurrentRiddle().CheckAnswer(answer);
+            if (State != HuntState.InProgress)
+                throw new InvalidOperationException("Game not in progress.");
+
+            var result = CurrentRiddle.CheckAnswer(answer);
             if (result == true)
                 ++currentRiddle;
             return result;
+        }
+
+        public IList<RiddleState> RiddleStates
+        {
+            get
+            {
+                return riddles.Select(r => new RiddleState(r.Question, riddles.IndexOf(r) < currentRiddle)).ToList();
+            }
+        }
+
+        public class RiddleState
+        {
+            public RiddleState(string question, bool answered)
+            {
+                Question = question;
+                Answered = answered;
+            }
+
+            public string Question { get; private set; }
+            public bool Answered { get; private set; }
+        }
+
+        public HuntState State
+        {
+            get
+            {
+                if (currentRiddle == riddles.Count)
+                    return HuntState.Completed;
+                return HuntState.InProgress;
+            }
         }
     }
 }
---
+++ HangoverHunt/HuntState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HangoverHunt
+{
+    public enum HuntState
+    {
+        InProgress,
+        Completed
+    }
+}
